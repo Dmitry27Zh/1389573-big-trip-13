c01a0bf35ba793361c5018999beab6d9f0726223
Dmitry <dmitry27zh@gmail.com>
2020-12-07 12:48:18 +0300

    компоненты переведены в классы

--- src/main.js
+++ src/main.js
@@ -1,16 +1,18 @@
-import {createTripInfoTemplate} from './view/trip-info';
-import {createTripTitleTemplate} from './view/trip-title';
-import {createTripDatesTemplate} from './view/trip-dates';
-import {createTripCostTemplate} from './view/trip-cost';
-import {createMenuTemplate} from './view/menu';
-import {createFiltersTemplate} from './view/filters';
-import {createSortTemplate} from './view/sort';
-import {createEventsListTemplate} from './view/events-list';
-import {createPointTemplate} from './view/point';
+import {TripInfoView} from './view/trip-info';
+import {TripTitleView} from './view/trip-title';
+import {TripDatesView} from './view/trip-dates';
+import {TripCostView} from './view/trip-cost';
+import {MenuView} from './view/menu';
+import {FiltersView} from './view/filters';
+import {SortView} from './view/sort';
+import {EventsListView} from './view/events-list';
+import {PointView} from './view/point';
 import {createEditPointTemplate} from './view/edit-point';
 import {generatePoint} from './mock/point';
 import {generateOffersToTypes} from './mock/point';
 import {generateInfoToDestinations} from './mock/point';
+import {render} from './utils';
+import {RenderPositions} from './const';
 
 const POINTS_QUANTITY = 15;
 
@@ -18,36 +20,26 @@ const offersToTypes = generateOffersToTypes();
 const infoToDestinations = generateInfoToDestinations();
 const points = new Array(POINTS_QUANTITY).fill().map(() => generatePoint(offersToTypes));
 
-const render = (container, template, place) => {
-  container.insertAdjacentHTML(place, template);
-};
 
 const tripMainElement = document.querySelector(`.trip-main`);
 
-render(tripMainElement, createTripInfoTemplate(), `afterBegin`);
+const tripInfoComponent = new TripInfoView();
+render(tripMainElement, tripInfoComponent.getElement(), RenderPositions.AFTERBEGIN);
 
-const tripInfoELement = tripMainElement.querySelector(`.trip-info`);
-const tripInfoMainElement = tripInfoELement.querySelector(`.trip-info__main`);
+const tripInfoMainElement = tripInfoComponent.getElement().querySelector(`.trip-info__main`);
 
-render(tripInfoMainElement, createTripTitleTemplate(), `afterBegin`);
-render(tripInfoMainElement, createTripDatesTemplate(), `beforeEnd`);
-render(tripInfoELement, createTripCostTemplate(), `beforeEnd`);
+render(tripInfoMainElement, new TripTitleView().getElement(), RenderPositions.AFTERBEGIN);
+render(tripInfoMainElement, new TripDatesView().getElement(), RenderPositions.BEFOREEND);
+render(tripInfoComponent.getElement(), new TripCostView().getElement(), RenderPositions.BEFOREEND);
 
 const tripControlsElement = tripMainElement.querySelector(`.trip-controls`);
 
-render(tripControlsElement, createMenuTemplate(), `beforeEnd`);
-render(tripControlsElement, createFiltersTemplate(), `beforeEnd`);
+render(tripControlsElement, new MenuView().getElement(), RenderPositions.AFTERBEGIN);
+render(tripControlsElement, new FiltersView().getElement(), RenderPositions.BEFOREEND);
 
 const tripEventsElement = document.querySelector(`.trip-events`);
+render(tripEventsElement, new SortView().getElement(), RenderPositions.BEFOREEND);
+const eventsListComponent = new EventsListView();
+render(tripEventsElement, eventsListComponent.getElement(), RenderPositions.BEFOREEND);
 
-render(tripEventsElement, createSortTemplate(), `beforeEnd`);
-render(tripEventsElement, createEventsListTemplate(), `beforeEnd`);
-
-const tripEventsListElement = tripEventsElement.querySelector(`.trip-events__list`);
-
-points.forEach((point) => {
-  render(tripEventsListElement, createPointTemplate(point, offersToTypes[point.type]), `beforeEnd`);
-});
-
-render(tripEventsListElement, createEditPointTemplate(offersToTypes, points[0], infoToDestinations[points[0].destination]), `afterBegin`);
-render(tripEventsListElement, createEditPointTemplate(offersToTypes), `afterBegin`);
+points.forEach((point) => render(eventsListComponent.getElement(), new PointView(point, offersToTypes[point.type]).getElement(), RenderPositions.BEFOREEND));
